361ce68db973e6fc6eaa68ec2d465fe518621b7d
Junbeom Wi <minkwns@naver.com>
2020-08-22 17:04:35 +0900

    Update file

---
+++ src/main/kotlin/com/example/demo/Models/AttributeFilter.kt
@@ -0,0 +1,22 @@
+package com.example.demo.Models
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class AttributeFilter(
+    @JsonProperty("schema_id") var SchemaId: String?,
+
+    @JsonProperty("schema_issuer_did") var SchemaIssuerDid: String?,
+
+    @JsonProperty("schema_name") var SchemaName: String?,
+
+    @JsonProperty("schema_version") var SchemaVersion: String?,
+
+    @JsonProperty("issuer_did") var IssuerDid: String?,
+
+    @JsonProperty("cred_def_id") var CredentialDefinitionId: String?
+)
+
+
+
