2c6af82d3ee6eaab520a5cbfbe2ecf8f2c5bb2c7
jinwoo Bae <wlsdn3578@gmail.com>
2020-08-23 21:38:12 +0900

    Update Models/RequestedAttribute.kt

---
+++ src/main/kotlin/com/example/demo/Models/RequestedAttribute.kt
@@ -0,0 +1,14 @@
+package com.example.demo.models
+
+import com.example.demo.Models.AttributeFilter
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class RequestedAttribute(
+        var name:String,
+
+        var label:String,
+
+        @JsonProperty("restrictions") var Restrictions: List<AttributeFilter>
+)
