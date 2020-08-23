b1efea54fa2cef6cb40f6725dad16bc14e4f8505
jinwoo Bae <wlsdn3578@gmail.com>
2020-08-23 21:47:16 +0900

    Update Models/RevocationInterval.kt

---
+++ src/main/kotlin/com/example/demo/Models/RevocationInterval.kt
@@ -0,0 +1,11 @@
+package com.example.demo.models
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class RevocationInterval(
+    var From: Long,
+
+    var To: Long
+)
