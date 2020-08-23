a11ded9d855e3a340ff101cde49d9a5f24a11a99
jinwoo Bae <wlsdn3578@gmail.com>
2020-08-23 21:51:37 +0900

    Update Models/RequestedPredicate.kt

---
+++ src/main/kotlin/com/example/demo/Models/RequestedPredicate.kt
@@ -0,0 +1,19 @@
+package com.example.demo.models
+
+import com.example.demo.Models.AttributeFilter
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonProperty
+
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class RequestedPredicate(
+    var Name : String,
+
+    var label : String,
+
+    var Restrictions : List<AttributeFilter>,
+
+    @JsonProperty("p_value") var PValue : String,
+
+    @JsonProperty("p_type") var PType : String
+)
