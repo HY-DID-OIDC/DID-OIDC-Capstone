d5a6b410e4375514e1ce0b28c1110007d75dec85
jinwoo Bae <wlsdn3578@gmail.com>
2020-08-24 14:58:56 +0900

    Update Models/ProofAttribute

---
+++ src/main/kotlin/com/example/demo/Models/ProofAttribute.kt
@@ -0,0 +1,12 @@
+package com.example.demo.Models
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.JsonNode
+
+
+data class ProofAttribute(
+    @JsonProperty("raw") var Raw: String,
+
+    @get:JsonAnyGetter var Rest:Map<String, JsonNode> =LinkedHashMap()
+)
