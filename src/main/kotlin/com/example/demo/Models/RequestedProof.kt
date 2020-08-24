45851f5971624cf41862517b380ed4f888e427c5
jinwoo Bae <wlsdn3578@gmail.com>
2020-08-24 14:52:47 +0900

    Update Models/ReuqestedProof.kt

--- src/main/kotlin/com/example/demo/Models/RequestedProof.kt
+++ src/main/kotlin/com/example/demo/Models/RequestedProof.kt
@@ -2,11 +2,11 @@ package com.example.demo.Models
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.core.JsonToken
 import com.example.demo.Models.ProofAttribute
+import com.fasterxml.jackson.databind.JsonNode
 
 data class RequestedProof(
     @JsonProperty("revealed_attrs") var RevealedAttributes : Map<String, ProofAttribute> = LinkedHashMap(),
 
-    @get:JsonAnyGetter var Rest:Map<String, JsonToken> =LinkedHashMap()
+    @get:JsonAnyGetter var Rest:Map<String, JsonNode> =LinkedHashMap()
 )
