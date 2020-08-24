97ef1044348e6e3280ec43821b13b5670ade08dc
dytlgozj <dytlgozj@naver.com>
2020-08-25 01:41:08 +0900

    Update Models/PresentationRequest.kt

--- src/main/kotlin/com/example/demo/Models/PresentationRequest_v_1_0.kt
+++ src/main/kotlin/com/example/demo/Models/PresentationRequest_v_1_0.kt
@@ -1,11 +1,9 @@
 package com.example.demo.Models
 
-import java.util.Dictionary
-
 data class PresentationRequest_v_1_0(
     var name: String,
     var version: String,
     var nonRevoked: RevocationInterval,
-    var requestedAttributes: Dictionary<String, RequestedAttribute>,
-    var requestedPredicates: Dictionary<String, RequestedPredicate>
+    var requestedAttributes: Map<String, RequestedAttribute> = LinkedHashMap(),
+    var requestedPredicates: Map<String, RequestedPredicate> = LinkedHashMap()
 )
