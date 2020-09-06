f8849e7ed1a213c76ced67fdad8c82c491894f11
Junbeom Wi <minkwns@naver.com>
2020-09-07 00:32:09 +0900

    update PresentationAttachment.kt

--- src/main/kotlin/com/capstone/didauthoidc/models/PresentationAttachment.kt
+++ src/main/kotlin/com/capstone/didauthoidc/models/PresentationAttachment.kt
@@ -1,11 +1,17 @@
-import java.util.*
+package com.capstone.didauthoidc.models
+
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.Dictionary
+
 
 data class PresentationAttachment(
-    @JsonProperty("@id") var Id: String,
+    @JsonProperty("@id")
+    var Id: String,
 
-    @JsonProperty("mime-type") var MimeType: String,
+    @JsonProperty("mime-type")
+    var MimeType: String,
 
-    @JsonProperty("data") var Data: Dictionary<String, String>
+    @JsonProperty("data")
+    var Data: Dictionary<String, String>
 )
 
