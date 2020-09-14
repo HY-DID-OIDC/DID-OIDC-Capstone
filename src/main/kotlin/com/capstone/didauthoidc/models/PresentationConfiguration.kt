c73915b134dfe4340f8d98e0ba8bab204760a819
dytlgozj <dytlgozj@naver.com>
2020-09-14 20:40:26 +0900

    VC 최상단 계층모델 Models/PresentationConfiguration.kt를 작성

--- src/main/kotlin/com/capstone/didauthoidc/models/PresentationConfiguration.kt
+++ src/main/kotlin/com/capstone/didauthoidc/models/PresentationConfiguration.kt
@@ -11,35 +11,5 @@ data class PresentationConfiguration (
     var SubjectIdentifier: String,
     var _configuration: String?,
     @JsonProperty("configuration")
-    var Configuration: PresentationRequestConfiguration?)
-{
-    val mapper = jacksonObjectMapper()
-    var safeConfiguration: PresentationRequestConfiguration?
-        get() = if (_configuration == null) null else mapper.readValue<PresentationRequestConfiguration>(_configuration!!)
-        set(value) {
-            _configuration = mapper.writeValueAsString(value)
-        }
-}
-
-fun main() {
-    var attributeFilter = AttributeFilter("this", "is", "a", "test code", "ok?", "ok.")
-
-    var attributeFilters = ArrayList<AttributeFilter>()
-    attributeFilters.add(attributeFilter)
-
-    var requestedAttribute = RequestedAttribute("email", "null", attributeFilters)
-    var requestedAttributes = ArrayList<RequestedAttribute>()
-    requestedAttributes.add(requestedAttribute)
-
-    var requestedPredicate = RequestedPredicate("emailPredicate", "null", attributeFilters, "thisIsPVlaue", "this is p_type")
-    var requestedPredicates = ArrayList<RequestedPredicate>()
-    requestedPredicates.add(requestedPredicate)
-
-    var presentationRequestConfiguration = PresentationRequestConfiguration("Basic Proof", "1.0", requestedAttributes, requestedPredicates)
-
-    var presentationConfiguration = PresentationConfiguration("test-request-config", "email", null, presentationRequestConfiguration)
-
-    presentationConfiguration.safeConfiguration = presentationRequestConfiguration
-    println(presentationConfiguration.safeConfiguration)
-    println(presentationConfiguration)
-}
+    var Configuration: PresentationRequestConfiguration?
+)
