8b4f5797308ce3db2fdde58e69e2d010d33370e2
dytlgozj <dytlgozj@naver.com>
2020-09-14 20:34:01 +0900

    긴 Url을 globally unique한 UUID를 이용한 짧은 Url로 바꾸기 위해 Services/UrlShortenerService 작성

---
+++ src/main/kotlin/com/capstone/didauthoidc/services/UrlShortenerService.kt
@@ -0,0 +1,12 @@
+package com.capstone.didauthoidc.services
+
+import java.util.UUID
+
+class UrlShortenerService {
+    fun createShortUrl(url: String) : String {
+/*        TODO : 일반url과 shorturl을 짝지어서 MappedUrl 클래스로 저장하여 DB에 저장해야함.
+        그래야 나중에 shorturl을 가지고 일반url을 get할수있음. */
+        val uuid: String = UUID.randomUUID().toString();
+        return "http://localhost:5000/url/$uuid"
+    }
+}
