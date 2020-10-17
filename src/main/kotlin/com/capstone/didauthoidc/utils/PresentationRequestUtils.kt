b12f8d674aae5e42dd3862cc20aca413088b94a4
dytlgozj <dytlgozj@naver.com>
2020-10-17 16:53:08 +0900

    PresentationRequestUtils에 extractIndyPresentationRequest()를 추가

--- src/main/kotlin/com/capstone/didauthoidc/utils/PresentationRequestUtils.kt
+++ src/main/kotlin/com/capstone/didauthoidc/utils/PresentationRequestUtils.kt
@@ -2,6 +2,7 @@ package com.capstone.didauthoidc.utils
 
 import com.capstone.didauthoidc.models.PresentationAttachment
 import com.capstone.didauthoidc.models.PresentationRequest
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.util.Base64
 
 class PresentationRequestUtils {
@@ -19,6 +20,24 @@ class PresentationRequestUtils {
             attachment.data["base64"] = base64Payload
 
             return mutableListOf(attachment)
+        }
+
+        fun extractIndyPresentationRequest(
+            presentationAttachments: MutableList<PresentationAttachment>
+        ): PresentationRequest {
+            var presentationRequest: PresentationRequest? = null
+
+            for (attachment in presentationAttachments) {
+                if(attachment.id.equals("libindy-request-presentation-0")) {
+                    presentationRequest = attachment.data["base64"]?.let {
+                        OurJacksonObjectMapper.getMapper().readValue(
+                            it
+                        )
+                    }
+                }
+            }
+
+            return presentationRequest!!
         }
     }
 }
