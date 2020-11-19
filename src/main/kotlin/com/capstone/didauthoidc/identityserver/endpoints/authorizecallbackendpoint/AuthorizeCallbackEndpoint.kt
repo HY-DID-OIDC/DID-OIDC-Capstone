6170efc7fd62e491e3812e71174f2a746ebfd97d
dytlgozj <dytlgozj@naver.com>
2020-11-19 23:11:50 +0900

    generatePresentationRequest()의 테스트코드에 대한 설명 추가

--- src/main/kotlin/com/capstone/didauthoidc/identityserver/endpoints/authorizecallbackendpoint/AuthorizeCallbackEndpoint.kt
+++ src/main/kotlin/com/capstone/didauthoidc/identityserver/endpoints/authorizecallbackendpoint/AuthorizeCallbackEndpoint.kt
@@ -24,18 +24,25 @@ class AuthorizeCallbackEndpoint {
     @RequestMapping("/vc/connect/callback", method = arrayOf(RequestMethod.POST))
     fun processAsync(@RequestParam param: MultiValueMap<String, String>, model: Model): String {
 
-        val attributeFilter: AttributeFilter = AttributeFilter(schemaName = "verified-email", issuerDid = "MTYqmTBoLT7KLP5RNfgK3b")
-        val requestedAttribute: RequestedAttribute = RequestedAttribute(name = "email")
+        // generatePresentationRequest 임시 테스트 코드 시작
+        val attributeFilter =
+            AttributeFilter(schemaName = "verified-email", issuerDid = "MTYqmTBoLT7KLP5RNfgK3b")
+        val requestedAttribute = RequestedAttribute(name = "email")
         requestedAttribute.restrictions?.add(attributeFilter)
         val presentationRequestConfiguration = PresentationRequestConfiguration("verified-email", "1.0")
         presentationRequestConfiguration.requestedAttributes.add(requestedAttribute)
 
-        val configuration: String = OurJacksonObjectMapper.getMapper().writeValueAsString(presentationRequestConfiguration)
+        val configuration: String =
+            OurJacksonObjectMapper.getMapper().writeValueAsString(presentationRequestConfiguration)
         println("[DEBUG] : $configuration")
 
-        val jsonRequestBody: String = PresentationRequestUtils.generatePresentationRequest(presentationRequestConfiguration)
+        // 위에서 만든 presentationRequestConfiguration을 파라미터로 전달하며 generatePresentationRequest를 호출한다.
+        val jsonRequestBody: String =
+            PresentationRequestUtils.generatePresentationRequest(presentationRequestConfiguration)
         println("[DEBUG] : $jsonRequestBody")
+        // generatePresentationRequest 임시 테스트 코드 끝
 
+        // 원본 코드 시작
         // 원래 코드는 이 코드이지만, 지금은 하드코딩 하겠다.
         val sessionId: String = param.getValue(IdentityConstants.ChallengeIdQueryParameterName).toString()
         val presentationRecordId = "test-request-config"
@@ -78,11 +85,17 @@ class AuthorizeCallbackEndpoint {
 
         // 원래는 Session DB에서 가져와야 하지만, 지금은 하드코딩 하겠다.
 //        var session: AuthSession? = sessionStorageService.FindByPresentationIdAsync(sessionId)
-        var session: AuthSession = AuthSession(presentationRequestId = sessionId, presentationRecordId = presentationRecordId, presentationRequest = presentationRequest, requestParameters = requestParameters)
+        var session: AuthSession = AuthSession(
+            presentationRequestId = sessionId,
+            presentationRecordId = presentationRecordId,
+            presentationRequest = presentationRequest,
+            requestParameters = requestParameters
+        )
 
         // response_type이 code이면 url에 code를 파라미터로 붙여서 넘겨준다. state도 있다면 state도 url에 붙인다.
         if (session.requestParameters[IdentityConstants.ResponseTypeUriParameterName] == "code") {
-            var url: String = "${session.requestParameters[IdentityConstants.RedirectUriParameterName]}?code=${session.id}"
+            var url: String =
+                "${session.requestParameters[IdentityConstants.RedirectUriParameterName]}?code=${session.id}"
 
             if (session.requestParameters.contains(IdentityConstants.StateParameterName))
                 url += "&state=${session.requestParameters[IdentityConstants.StateParameterName]}"
